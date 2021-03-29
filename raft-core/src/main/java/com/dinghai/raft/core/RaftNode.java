c42356c030ca275193b00327bea3c3a4fd18baba
ding-hai <dhairoot@gmail.com>
2021-03-29 16:59:36 +0800

    basic code of RaftPeer and RaftNode

--- raft-core/src/main/java/com/dinghai/raft/core/RaftNode.java
+++ raft-core/src/main/java/com/dinghai/raft/core/RaftNode.java
@@ -1,8 +1,45 @@
 package com.dinghai.raft.core;
 
+import com.dinghai.raft.core.proto.RaftMessage;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 负责具体的交互过程
  */
 public class RaftNode {
+
+    public enum State {
+        FOLLOWER,
+        CANDIDATE,
+        LEADER
+    }
+
+    private RaftMessage.Configuration configuration;
+    private RaftMessage.Server localServer; //当前节点对应的server信息
+    private Map<Integer, RaftPeer> peerMap = new HashMap<>(); // id->peer. peerMap维护的是除了当前这个server之前的其他server对应的peer
+
+    private State state = State.FOLLOWER;
+
+    private int leadId;
+    private int voteFor;
+
+    private long currentTerm;
+    private long commitIndex;
+    private long lastAppliedIndex;
+
+    public RaftNode(List<RaftMessage.Server> servers, RaftMessage.Server localServer) {
+        RaftMessage.Configuration.Builder configurationBuilder = RaftMessage.Configuration.newBuilder();
+        configurationBuilder.addAllServers(servers);
+        this.configuration = configurationBuilder.build();
+        this.localServer = localServer;
+    }
+
+    public void init() {
+
+    }
+
 
 }
--- raft-core/src/main/java/com/dinghai/raft/core/RaftPeer.java
+++ raft-core/src/main/java/com/dinghai/raft/core/RaftPeer.java
@@ -1,8 +1,79 @@
 package com.dinghai.raft.core;
 
+import com.baidu.brpc.client.BrpcProxy;
+import com.baidu.brpc.client.RpcClient;
+import com.baidu.brpc.client.channel.Endpoint;
+import com.dinghai.raft.core.proto.RaftMessage;
+import com.dinghai.raft.core.service.RaftConsensusService;
+import com.dinghai.raft.core.service.RaftConsensusServiceAsync;
+
 /**
  * 维护节点信息，不负责具体交互
+ * 每一个RaftPeer对应一个server的ip和端口以及和这个server通信的rpc client和service
  */
 public class RaftPeer {
+    private RaftMessage.Server server; //当前peer对应的server信息
 
+    private RpcClient rpcClient; // 负责和当前peer对应的server交互的rpc client
+    private RaftConsensusService raftConsensusService; // 同步调用的service
+    private RaftConsensusServiceAsync raftConsensusServiceAsync; // 异步调用的service
+
+    private long matchIndex;
+    private long nextIndex;
+
+    public RaftPeer(RaftMessage.Server server) {
+        this.server = server;
+        this.rpcClient = new RpcClient(new Endpoint(
+                server.getEndpoint().getHost(),
+                server.getEndpoint().getPort()));
+        this.raftConsensusService = BrpcProxy.getProxy(this.rpcClient, RaftConsensusService.class);
+    }
+
+    public RaftMessage.Server getServer() {
+        return server;
+    }
+
+    public void setServer(RaftMessage.Server server) {
+        this.server = server;
+    }
+
+    public RpcClient getRpcClient() {
+        return rpcClient;
+    }
+
+    public void setRpcClient(RpcClient rpcClient) {
+        this.rpcClient = rpcClient;
+    }
+
+    public RaftConsensusService getRaftConsensusService() {
+        return raftConsensusService;
+    }
+
+    public void setRaftConsensusService(RaftConsensusService raftConsensusService) {
+        this.raftConsensusService = raftConsensusService;
+    }
+
+    public RaftConsensusServiceAsync getRaftConsensusServiceAsync() {
+        return raftConsensusServiceAsync;
+    }
+
+    public void setRaftConsensusServiceAsync(RaftConsensusServiceAsync raftConsensusServiceAsync) {
+        this.raftConsensusServiceAsync = raftConsensusServiceAsync;
+    }
+
+    public long getMatchIndex() {
+        return matchIndex;
+    }
+
+    public void setMatchIndex(long matchIndex) {
+        this.matchIndex = matchIndex;
+    }
+
+    public long getNextIndex() {
+        return nextIndex;
+    }
+
+    public void setNextIndex(long nextIndex) {
+        this.nextIndex = nextIndex;
+    }
 }
