445b7eb1135173a6f75dc53278a0475b5fcb9558
ding-hai <dhairoot@gmail.com>
2021-03-29 17:11:02 +0800

    add the item of server list into peerMap expect local server

--- raft-core/src/main/java/com/dinghai/raft/core/RaftNode.java
+++ raft-core/src/main/java/com/dinghai/raft/core/RaftNode.java
@@ -18,7 +18,7 @@ public class RaftNode {
     }
 
     private RaftMessage.Configuration configuration;
-    private RaftMessage.Server localServer; //当前节点对应的server信息
+    private final RaftMessage.Server localServer; //当前节点对应的server信息
     private Map<Integer, RaftPeer> peerMap = new HashMap<>(); // id->peer. peerMap维护的是除了当前这个server之前的其他server对应的peer
 
     private State state = State.FOLLOWER;
@@ -38,7 +38,14 @@ public class RaftNode {
     }
 
     public void init() {
-
+        List<RaftMessage.Server> serverList = configuration.getServersList();
+        for (RaftMessage.Server server : serverList) {
+            if(localServer.equals(server)){
+                continue;
+            }
+            RaftPeer peer = new RaftPeer(server);
+            peerMap.put(server.getServerId(), peer);
+        }
     }
 
 
